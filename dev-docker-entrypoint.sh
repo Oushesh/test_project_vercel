d39f19329a8ed326c0810c5e0f22ea53a31b190a
oushesh <oushesh@gmail.com>
2023-09-06 19:42:35 +0400

    dev-docker-entrypoint.sh

--- dev-docker-entrypoint.sh
+++ dev-docker-entrypoint.sh
@@ -7,9 +7,3 @@ python manage.py migrate
 # Start Backend server
 echo "Start server"
 python manage.py runserver 0.0.0.0:8000
-
-# Start Frontend
-#echo "Start frontend"
-#cd frontend
-#yarn run build
-#yarn run start
