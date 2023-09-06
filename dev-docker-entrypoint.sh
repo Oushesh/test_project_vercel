b0136deff43d6feb0347f1202852d422a779a411
oushesh <oushesh@gmail.com>
2023-09-06 20:00:30 +0400

    added collectstatinc to dev-docker-entrypoint.sh

--- dev-docker-entrypoint.sh
+++ dev-docker-entrypoint.sh
@@ -4,6 +4,9 @@
 echo "Apply database migrations"
 python manage.py migrate
 
+# Heroku collecstatic --noinput
+python manage.py collectstatic --noinput
+
 # Start Backend server
 echo "Start server"
 python manage.py runserver 0.0.0.0:8000
