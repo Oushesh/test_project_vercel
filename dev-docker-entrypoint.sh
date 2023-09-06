67cf420cdbce3046686de6a9cbf7c75fe08cd082
oushesh <oushesh@gmail.com>
2023-09-06 19:29:39 +0400

    updated: proper entrypoint docker-compose.yml

--- dev-docker-entrypoint.sh
+++ dev-docker-entrypoint.sh
@@ -1,9 +1,15 @@
-version: '3.9'
-services:
-  backend:
-    build:
-      context: .
-      dockerfile: dev.Dockerfile
-    ports:
-      - "8000:8000"
+#!/bin/bash
 
+# Apply database migrations
+echo "Apply database migrations"
+python manage.py migrate
+
+# Start Backend server
+echo "Start server"
+python manage.py runserver 0.0.0.0:8000
+
+# Start Frontend
+#echo "Start frontend"
+#cd frontend
+#yarn run build
+#yarn run start
