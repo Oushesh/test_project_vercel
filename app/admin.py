d1f02a8d958e7a35a5c08791c93a538ae95e4cb9
oushesh <oushesh@gmail.com>
2023-09-07 16:38:21 +0400

    added static_root=static/

--- app/admin.py
+++ app/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 
 # Register your models here.
-# Register your models here.
 from .models import BlogPost
 admin.site.register(BlogPost) #admin registerign the Django Data Model
--- start_project/asgi.py
+++ start_project/asgi.py
@@ -6,7 +6,6 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 """
-
 import os
 
 from django.core.asgi import get_asgi_application
@@ -14,3 +13,5 @@ from django.core.asgi import get_asgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "start_project.settings.dev")
 
 application = get_asgi_application()
+
+#For deployment on vercel we would
